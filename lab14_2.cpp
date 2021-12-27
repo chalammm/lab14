9569efa998cde27697b52e907a9f2e7babcf5ed9
chalammm <tipanan_ji@cmu.ac.th>
2021-12-28 02:09:16 +0700

    completedlab14-2.cpp

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -20,4 +20,32 @@ int main()
         updateImage(image,s,x,y);
     }while(s != 0 || x != 0 || y != 0);
     return 0;
+} 
+
+void showImage(const bool pic[][M]) 
+{
+    cout << "------------------------------------------------------------------------\n";
+    for(int q = 0; q < N; q++){
+        cout << "|";
+        for(int w = 0; w < M ; w++){
+            if(pic[q][w] == 0) cout << " ";
+            if(pic[q][w] == 1) cout << "*";
+        }
+        cout << "|" << endl;
+    }
+    cout << "------------------------------------------------------------------------\n";
+} 
+
+void updateImage(bool lol[][M],int a,int b,int c)
+{
+    for(int q = 0; q < N; q++)
+    {
+        for(int w = 0; w < M ; w++)
+        {
+         if (sqrt(pow(q - b, 2) + pow(w - c, 2)) + 1 <= a )
+         {
+             lol[q][w]=1;
+         }
+        }
+    }
 }
